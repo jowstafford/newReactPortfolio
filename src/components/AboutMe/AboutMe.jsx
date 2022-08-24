febd65c949064bbe97aee11ca1bf31ccf975a487
jowstafford <91708768+jowstafford@users.noreply.github.com>
2022-08-23 21:15:35 -0500

    made tremendous progress with the aboutMe circle animations

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -11,22 +11,42 @@ export default function AboutMe() {
               <circle cx="70px" cy="70px" r="70px"></circle>
               <circle cx="70px" cy="70px" r="70px"></circle>
             </svg>
+            <div class="number">
+              <h2>
+                85<span>%</span>
+              </h2>
+              <p>Html</p>
+            </div>
           </div>
         </div>
         <div class="card">
           <div class="percent">
+            <div class="dot"></div>
             <svg>
               <circle cx="70px" cy="70px" r="70px"></circle>
               <circle cx="70px" cy="70px" r="70px"></circle>
             </svg>
+            <div class="number">
+              <h2>
+                85<span>%</span>
+              </h2>
+              <p>Html</p>
+            </div>
           </div>
         </div>
         <div class="card">
           <div class="percent">
+            <div class="dot"></div>
             <svg>
               <circle cx="70px" cy="70px" r="70px"></circle>
               <circle cx="70px" cy="70px" r="70px"></circle>
             </svg>
+            <div class="number">
+              <h2>
+                85<span>%</span>
+              </h2>
+              <p>Html</p>
+            </div>
           </div>
         </div>
       </div>
